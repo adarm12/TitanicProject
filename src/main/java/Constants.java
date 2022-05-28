bdfb52b0898387bf3dde9e32d6e2d559c1e23619
Shanitkk <Shanitk2020@gmail.com>
2022-05-28 19:44:52 +0300

    .

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -3,12 +3,13 @@ public class Constants {
     public static final int WINDOW_WIDTH = 1400;
     public static final int WINDOW_HEIGHT = 900;
 
-    public static final int PASSENGER_ID = 0;
+    public static final int PASSENGER_ID = 0, PASSENGER_SURVIVED = 1, PASSENGER_P_CLASS = 2,
+            PASSENGER_FIRST_NAME = 3, PASSENGER_LAST_NAME = 4, PASSENGER_SEX = 5, PASSENGER_AGE = 6,
+            PASSENGER_SIB_SP = 7, PASSENGER_PARCH = 8, PASSENGER_TICKET = 9, PASSENGER_FARE = 10, PASSENGER_CABIN = 11,
+            PASSENGER_EMBARKED = 12;
 
 
-
-
-    public static final String[] PASSENGER_CLASS_OPTIONS = { "All", "1st", "2nd", "3rd"};
+    public static final String[] PASSENGER_CLASS_OPTIONS = {"All", "1st", "2nd", "3rd"};
     public static final int MARGIN_FROM_TOP = 25;
     public static final int MARGIN_FROM_LEFT = 25;
     public static final int LABEL_WIDTH = 150;
@@ -18,4 +19,4 @@ public class Constants {
 
     public static final char COMMA = ',', DOT = '.';
     public static final int LETTER_AFTER_DOT = 2;
-    }
+}
