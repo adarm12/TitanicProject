827ecfdef559ba86e36b9f724898b2e043c2eb44
adarm12 <adarm1234@gmail.com>
2022-05-30 22:11:42 +0300

    1

--- src/main/java/createNew.java
+++ src/main/java/createNew.java
@@ -4,6 +4,20 @@ import java.awt.*;
 public class createNew {
 
 
+    public  static JTextField newTextField(int x, int y, int width, int height) {
+        Font myFont = new Font("Gisha", Font.BOLD, 18);
+        JTextField textField = new JTextField();
+        textField.setBounds(x, y, width, height);
+        textField.setFont(myFont);
+        return textField;
+    }
+}
+import javax.swing.*;
+import java.awt.*;
+
+public class createNew {
+
+
 //    public  static JTextField newTextField(int x, int y, int width, int height) {
 //        Font myFont = new Font("Gisha", Font.BOLD, 18);
 //        JTextField textField = new JTextField();
